481f278efd09d16dd80511b4b5eaa046358aee3d
Antonio Yang <yanganto@gmail.com>
2020-06-02 13:17:24 +0800

    tmp

--- pallets/kitty/src/lib.rs
+++ pallets/kitty/src/lib.rs
@@ -2,15 +2,7 @@
 
 use balances;
 use codec::{Decode, Encode};
-/// A FRAME pallet template with necessary imports
-
-/// Feel free to remove or edit this file as needed.
-/// If you change the name of this file, make sure to update its references in runtime/src/lib.rs
-/// If you remove this file, you can remove those references
-
-/// For more guidance on Substrate FRAME, see the example pallet
-/// https://github.com/paritytech/substrate/blob/master/frame/example/src/lib.rs
-use frame_support::{decl_error, decl_event, decl_module, decl_storage, dispatch};
+use frame_support::{decl_error, decl_event, decl_module, decl_storage, dispatch, ensure};
 use frame_system::{self as system, ensure_signed};
 use sp_runtime::RuntimeDebug;
 
@@ -28,38 +20,23 @@ pub struct Kitty<Hash, Balance> {
     gen: u64,
 }
 
-/// The pallet's configuration trait.
 pub trait Trait: system::Trait + balances::Trait {
-    // Add other types and constants required to configure this pallet.
-
-    /// The overarching event type.
     type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
 }
 
-// This pallet's storage items.
 decl_storage! {
-    // It is important to update your storage name so that your pallet's
-    // storage items are isolated from other pallets.
-    // ---------------------------------vvvvvvvvvvvvvv
     trait Store for Module<T: Trait> as Kitty {
-        // Just a dummy storage item.
-        // Here we are declaring a StorageValue, `Something` as a Option<u32>
-        // `get(fn something)` is the default getter which returns either the stored `u32` or `None` if nothing stored
-        Something get(fn something): Option<u32>;
         OwnedKitty get(fn kitty_of_owner) : map hasher(identity) T::AccountId => Option<Kitty<T::Hash, T::Balance>>;
     }
 }
 
-// The pallet's events
 decl_event!(
     pub enum Event<T>
     where
         AccountId = <T as system::Trait>::AccountId,
+        Hash = <T as system::Trait>::Hash,
     {
-        /// Just a dummy event.
-        /// Event `Something` is declared with a parameter of the type `u32` and `AccountId`
-        /// To emit this event, we call the deposit function, from our runtime functions
-        SomethingStored(u32, AccountId),
+        Created(AccountId, Hash),
     }
 );
 
@@ -73,51 +50,18 @@ decl_error! {
     }
 }
 
-// The pallet's dispatchable functions.
 decl_module! {
-    /// The module declaration.
     pub struct Module<T: Trait> for enum Call where origin: T::Origin {
-        // Initializing errors
-        // this includes information about your errors in the node's metadata.
-        // it is needed only if you are using errors in your pallet
         type Error = Error<T>;
-
-        // Initializing events
-        // this is needed only if you are using events in your pallet
         fn deposit_event() = default;
 
-        /// Just a dummy entry point.
-        /// function that can be called by the external world as an extrinsics call
-        /// takes a parameter of the type `AccountId`, stores it, and emits an event
-        #[weight = 10_000]
-        pub fn do_something(origin, something: u32) -> dispatch::DispatchResult {
-            // Check it was signed and get the signer. See also: ensure_root and ensure_none
-            let who = ensure_signed(origin)?;
-
-            // Code to execute when something calls this.
-            // For example: the following line stores the passed in u32 in the storage
-            Something::put(something);
-
-            // Here we are raising the Something event
-            Self::deposit_event(RawEvent::SomethingStored(something, who));
+        #[weight = 0]
+        pub fn create_kitty(origin) -> dispatch::DispatchResult {
+            let nonce = <Nonce<T>>::get();
+            let random_hash = (<system::Module<T>>::random_seed(), &sender, nonce)
+                .using_encoded(<T as system::Trait>::Hashing::hash);
+            ensure!(!<KittyOwner<T>>::exists(random_hash), "Kitty already exists");
             Ok(())
-        }
-
-        /// Another dummy entry point.
-        /// takes no parameters, attempts to increment storage value, and possibly throws an error
-        #[weight = 10_000]
-        pub fn cause_error(origin) -> dispatch::DispatchResult {
-            // Check it was signed and get the signer. See also: ensure_root and ensure_none
-            let _who = ensure_signed(origin)?;
-
-            match Something::get() {
-                None => Err(Error::<T>::NoneValue)?,
-                Some(old) => {
-                    let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
-                    Something::put(new);
-                    Ok(())
-                },
-            }
         }
     }
 }
