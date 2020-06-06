9861e09938e73b53e1d75d425dd2ecd4faf546f6
Antonio Yang <yanganto@gmail.com>
2020-06-06 14:16:28 +0800

    create kitty

--- pallets/kitty/src/lib.rs
+++ pallets/kitty/src/lib.rs
@@ -2,9 +2,11 @@
 
 use balances;
 use codec::{Decode, Encode};
-use frame_support::{decl_error, decl_event, decl_module, decl_storage, dispatch, ensure};
+use frame_support::{decl_error, decl_event, decl_module, decl_storage, dispatch};
 use frame_system::{self as system, ensure_signed};
 use sp_runtime::RuntimeDebug;
+
+use crate::sp_api_hidden_includes_decl_storage::hidden_include::traits::Randomness;
 
 #[cfg(test)]
 mod mock;
@@ -26,7 +28,10 @@ pub trait Trait: system::Trait + balances::Trait {
 
 decl_storage! {
     trait Store for Module<T: Trait> as Kitty {
-        OwnedKitty get(fn kitty_of_owner) : map hasher(identity) T::AccountId => Option<Kitty<T::Hash, T::Balance>>;
+        OwnedKitty get(fn kitty_of_owner) : map hasher(identity) T::AccountId => Vec<T::Hash>;
+        KittyOwner get(fn owner_of) : map hasher(blake2_128_concat) T::Hash => Option<T::AccountId>;
+        Kitties get(fn kitties): map hasher(blake2_128_concat) T::Hash => Kitty<T::Hash, T::Balance>;
+
     }
 }
 
@@ -40,13 +45,10 @@ decl_event!(
     }
 );
 
-// The pallet's errors
 decl_error! {
     pub enum Error for Module<T: Trait> {
-        /// Value was None
-        NoneValue,
-        /// Value reached maximum and cannot be incremented further
-        StorageOverflow,
+        // the Kiity is unique
+        CreatedKittyExist,
     }
 }
 
@@ -57,10 +59,28 @@ decl_module! {
 
         #[weight = 0]
         pub fn create_kitty(origin) -> dispatch::DispatchResult {
-            let nonce = <Nonce<T>>::get();
-            let random_hash = (<system::Module<T>>::random_seed(), &sender, nonce)
-                .using_encoded(<T as system::Trait>::Hashing::hash);
-            ensure!(!<KittyOwner<T>>::exists(random_hash), "Kitty already exists");
+
+            let owner = ensure_signed(origin)?;
+
+            // add ramdomness_collective_flip pallet to create random hash
+            let r = <pallet_randomness_collective_flip::Module<T>>::random_seed();
+
+            if <KittyOwner<T>>::contains_key(r) {
+               return Err(<Error<T>>::CreatedKittyExist)?;
+            }
+
+
+            let new_kitty = Kitty {
+                id: r,
+                dna: r,
+                price: 0.into(),
+                gen: 0,
+            };
+
+            <Kitties<T>>::insert(r, new_kitty);
+            <KittyOwner<T>>::insert(r, &owner);
+            <OwnedKitty<T>>::mutate(&owner, |v| v.push(r));
+
             Ok(())
         }
     }
