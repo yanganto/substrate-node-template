8f9253fcea3a199135b771d57d6d9fdf54c16821
Antonio Yang <yanganto@gmail.com>
2020-10-16 17:02:31 +0800

    pallet: add name-reg

---
+++ pallets/name-registry/src/lib.rs
@@ -0,0 +1,79 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+/// A vanity name registering system resistant against frontrunning.
+///
+/// An unregistered name can be registered for a certain amount of time by locking a certain balance of an account.
+/// After the registration expires, the account loses ownership of the name and his balance is unlocked.
+/// The registration can be renewed by making an on-chain call to keep the name registered and balance locked.
+use frame_support::{decl_error, decl_event, decl_module, decl_storage, dispatch, traits::Get};
+use frame_system::ensure_signed;
+
+#[cfg(test)]
+mod mock;
+
+#[cfg(test)]
+mod tests;
+
+pub trait Trait: frame_system::Trait {
+    type Event: From<Event<Self>> + Into<<Self as frame_system::Trait>::Event>;
+}
+
+decl_storage! {
+    trait Store for Module<T: Trait> as TemplateModule {
+        Something get(fn something): Option<u32>;
+    }
+}
+
+decl_event!(
+    pub enum Event<T>
+    where
+        AccountId = <T as frame_system::Trait>::AccountId,
+    {
+        /// Event documentation should end with an array that provides descriptive names for event
+        /// parameters. [something, who]
+        SomethingStored(u32, AccountId),
+    }
+);
+
+decl_error! {
+    pub enum Error for Module<T: Trait> {
+        /// Error names should be descriptive.
+        NoneValue,
+        /// Errors should have helpful documentation associated with them.
+        StorageOverflow,
+    }
+}
+
+decl_module! {
+    pub struct Module<T: Trait> for enum Call where origin: T::Origin {
+        type Error = Error<T>;
+
+        fn deposit_event() = default;
+
+        /// An example dispatchable that takes a singles value as a parameter, writes the value to
+        /// storage and emits an event. This function must be dispatched by a signed extrinsic.
+        #[weight = 10_000 + T::DbWeight::get().writes(1)]
+        pub fn do_something(origin, something: u32) -> dispatch::DispatchResult {
+            let who = ensure_signed(origin)?;
+
+            Something::put(something);
+
+            Self::deposit_event(RawEvent::SomethingStored(something, who));
+            Ok(())
+        }
+
+        /// An example dispatchable that may throw a custom error.
+        #[weight = 10_000 + T::DbWeight::get().reads_writes(1,1)]
+        pub fn cause_error(origin) -> dispatch::DispatchResult {
+            let _who = ensure_signed(origin)?;
+
+            match Something::get() {
+                None => Err(Error::<T>::NoneValue)?,
+                Some(old) => {
+                    let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
+                    Something::put(new);
+                    Ok(())
+                },
+            }
+        }
+    }
+}
