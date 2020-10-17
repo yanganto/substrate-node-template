52823f23c998e92dece1a79382045003c67fd799
Antonio Yang <yanganto@gmail.com>
2020-10-17 18:35:45 +0800

    pallet: name-reg: add test case

--- pallets/name-registry/src/mock.rs
+++ pallets/name-registry/src/mock.rs
@@ -1,13 +1,15 @@
 use crate::{Module, Trait};
-use sp_core::H256;
 use frame_support::{impl_outer_origin, parameter_types, weights::Weight};
-use sp_runtime::{
-	traits::{BlakeTwo256, IdentityLookup}, testing::Header, Perbill,
-};
 use frame_system as system;
+use sp_core::H256;
+use sp_runtime::{
+    testing::Header,
+    traits::{BlakeTwo256, IdentityLookup},
+    Perbill,
+};
 
 impl_outer_origin! {
-	pub enum Origin for Test {}
+    pub enum Origin for Test {}
 }
 
 // Configure a mock runtime to test the pallet.
@@ -15,47 +17,81 @@ impl_outer_origin! {
 #[derive(Clone, Eq, PartialEq)]
 pub struct Test;
 parameter_types! {
-	pub const BlockHashCount: u64 = 250;
-	pub const MaximumBlockWeight: Weight = 1024;
-	pub const MaximumBlockLength: u32 = 2 * 1024;
-	pub const AvailableBlockRatio: Perbill = Perbill::from_percent(75);
+    pub const BlockHashCount: u64 = 250;
+    pub const MaximumBlockWeight: Weight = 1024;
+    pub const MaximumBlockLength: u32 = 2 * 1024;
+    pub const AvailableBlockRatio: Perbill = Perbill::from_percent(75);
+    pub const ExistentialDeposit: u64 = 1;
 }
 
 impl system::Trait for Test {
-	type BaseCallFilter = ();
-	type Origin = Origin;
-	type Call = ();
-	type Index = u64;
-	type BlockNumber = u64;
-	type Hash = H256;
-	type Hashing = BlakeTwo256;
-	type AccountId = u64;
-	type Lookup = IdentityLookup<Self::AccountId>;
-	type Header = Header;
-	type Event = ();
-	type BlockHashCount = BlockHashCount;
-	type MaximumBlockWeight = MaximumBlockWeight;
-	type DbWeight = ();
-	type BlockExecutionWeight = ();
-	type ExtrinsicBaseWeight = ();
-	type MaximumExtrinsicWeight = MaximumBlockWeight;
-	type MaximumBlockLength = MaximumBlockLength;
-	type AvailableBlockRatio = AvailableBlockRatio;
-	type Version = ();
-	type PalletInfo = ();
-	type AccountData = ();
-	type OnNewAccount = ();
-	type OnKilledAccount = ();
-	type SystemWeightInfo = ();
+    type BaseCallFilter = ();
+    type Origin = Origin;
+    type Call = ();
+    type Index = u64;
+    type BlockNumber = u64;
+    type Hash = H256;
+    type Hashing = BlakeTwo256;
+    type AccountId = u64;
+    type Lookup = IdentityLookup<Self::AccountId>;
+    type Header = Header;
+    type Event = ();
+    type BlockHashCount = BlockHashCount;
+    type MaximumBlockWeight = MaximumBlockWeight;
+    type DbWeight = ();
+    type BlockExecutionWeight = ();
+    type ExtrinsicBaseWeight = ();
+    type MaximumExtrinsicWeight = MaximumBlockWeight;
+    type MaximumBlockLength = MaximumBlockLength;
+    type AvailableBlockRatio = AvailableBlockRatio;
+    type Version = ();
+    type PalletInfo = ();
+    type AccountData = pallet_balances::AccountData<u64>;
+    type OnNewAccount = ();
+    type OnKilledAccount = ();
+    type SystemWeightInfo = ();
+}
+
+impl pallet_balances::Trait for Test {
+    type MaxLocks = ();
+    type Balance = u64;
+    type Event = ();
+    type DustRemoval = ();
+    type ExistentialDeposit = ExistentialDeposit;
+    type AccountStore = System;
+    type WeightInfo = ();
+}
+
+impl pallet_timestamp::Trait for Test {
+    type Moment = u64;
+    type OnTimestampSet = ();
+    type MinimumPeriod = ();
+    type WeightInfo = ();
 }
 
 impl Trait for Test {
-	type Event = ();
+    type Event = ();
+    type Currency = Balances;
 }
 
-pub type TemplateModule = Module<Test>;
+pub type NameRegisteredModule = Module<Test>;
+pub type Balances = pallet_balances::Module<Test>;
+pub type System = frame_system::Module<Test>;
+pub type Timestamp = pallet_timestamp::Module<Test>;
 
 // Build genesis storage according to the mock runtime.
 pub fn new_test_ext() -> sp_io::TestExternalities {
-	system::GenesisConfig::default().build_storage::<Test>().unwrap().into()
+    let mut t = frame_system::GenesisConfig::default()
+        .build_storage::<Test>()
+        .unwrap();
+    pallet_balances::GenesisConfig::<Test> {
+        balances: vec![
+            (1, u8::max_value() as u64),
+            (2, u16::max_value() as u64),
+            (3, u32::max_value() as u64),
+        ],
+    }
+    .assimilate_storage(&mut t)
+    .unwrap();
+    t.into()
 }
--- pallets/name-registry/src/tests.rs
+++ pallets/name-registry/src/tests.rs
@@ -1,23 +1,106 @@
-use crate::{Error, mock::*};
-use frame_support::{assert_ok, assert_noop};
+use crate::{mock::*, Error};
+use frame_support::{assert_noop, assert_ok};
 
 #[test]
-fn it_works_for_default_value() {
-	new_test_ext().execute_with(|| {
-		// Dispatch a signed extrinsic.
-		assert_ok!(TemplateModule::do_something(Origin::signed(1), 42));
-		// Read pallet storage and assert an expected result.
-		assert_eq!(TemplateModule::something(), Some(42));
-	});
+fn register_a_name() {
+    new_test_ext().execute_with(|| {
+        assert_eq!(
+            Balances::total_issuance(),
+            (u32::max_value() as u64) + (u16::max_value() as u64) + (u8::max_value() as u64)
+        );
+        let name = vec![1, 2, 3, 4];
+        Timestamp::set_timestamp(42);
+        assert_eq!(Balances::free_balance(&1), 255);
+        assert_ok!(NameRegisteredModule::register_name(
+            Origin::signed(1),
+            name.clone()
+        ));
+        let maybe_a_name = NameRegisteredModule::names(name.clone());
+        assert_eq!(maybe_a_name.is_some(), true);
+        assert_eq!(maybe_a_name.unwrap().expired, 604800042);
+        assert_eq!(Balances::free_balance(&1), 255 - (name.len() as u64));
+    });
 }
 
 #[test]
-fn correct_error_for_none_value() {
-	new_test_ext().execute_with(|| {
-		// Ensure the expected error is thrown when no value is present.
-		assert_noop!(
-			TemplateModule::cause_error(Origin::signed(1)),
-			Error::<Test>::NoneValue
-		);
-	});
+fn correct_error_for_register_twice() {
+    new_test_ext().execute_with(|| {
+        let name = vec![1, 2, 3, 4];
+        assert_eq!(Balances::free_balance(&1), 255);
+        assert_eq!(Balances::free_balance(&2), 65535);
+        assert_ok!(NameRegisteredModule::register_name(
+            Origin::signed(1),
+            name.clone()
+        ));
+        assert_noop!(
+            NameRegisteredModule::register_name(Origin::signed(2), name.clone()),
+            Error::<Test>::NameAlreadyRegistered
+        );
+        assert_eq!(Balances::free_balance(&1), 255 - (name.len() as u64));
+        assert_eq!(Balances::free_balance(&2), 65535);
+    });
+}
+
+#[test]
+fn correct_error_for_register_invalid_name() {
+    new_test_ext().execute_with(|| {
+        assert_noop!(
+            NameRegisteredModule::register_name(Origin::signed(2), vec![]),
+            Error::<Test>::NameInvalid
+        );
+    });
+}
+
+#[test]
+fn renew_a_registration() {
+    new_test_ext().execute_with(|| {
+        assert_ok!(NameRegisteredModule::register_name(
+            Origin::signed(1),
+            vec![1, 2, 3, 4]
+        ));
+        assert_ok!(NameRegisteredModule::renew_register_name(
+            Origin::signed(1),
+            vec![1, 2, 3, 4]
+        ));
+    });
+}
+
+#[test]
+fn correct_error_for_renew_registration_by_others() {
+    new_test_ext().execute_with(|| {
+        assert_ok!(NameRegisteredModule::register_name(
+            Origin::signed(1),
+            vec![1, 2, 3, 4]
+        ));
+        assert_noop!(
+            NameRegisteredModule::renew_register_name(Origin::signed(2), vec![1, 2, 3, 4]),
+            Error::<Test>::NameRegisteredByOther
+        );
+    });
+}
+
+#[test]
+fn correct_error_for_renew_a_nonexist_registration() {
+    new_test_ext().execute_with(|| {
+        Timestamp::set_timestamp(42);
+        assert_noop!(
+            NameRegisteredModule::renew_register_name(Origin::signed(1), vec![1, 2, 3, 4]),
+            Error::<Test>::NameNotExist
+        );
+    });
+}
+
+#[test]
+fn correct_error_for_unregister_a_name() {
+    new_test_ext().execute_with(|| {
+        let name = vec![1, 2, 3, 4];
+        assert_ok!(NameRegisteredModule::register_name(
+            Origin::signed(1),
+            name.clone()
+        ));
+        assert_noop!(
+            NameRegisteredModule::ungister_name(Origin::signed(1), name.clone()),
+            Error::<Test>::RegisteredTimeNotOver
+        );
+    });
 }
